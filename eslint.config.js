333f561710df401422e815063dafda56454ded8e
David Beale <david.beale@bealearts.co.uk>
2026-03-16 16:57:31 +0000

    Inc browser APIs

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import neostandard from 'neostandard'
 
 export default neostandard({
-  env: ['node', 'vitest'],
+  env: ['node', 'vitest', 'browser'],
   ignores: [...neostandard.resolveIgnoresFromGitignore()],
   noJsx: true,
   noStyle: true
